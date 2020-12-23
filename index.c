9242020e3e4afab6952fb51f7a2976c01b03f75f
lyubchenko-ivan <lubchenko2044@gmail.com>
2020-12-23 19:52:05 +0300

    add comment at index.c

--- index.c
+++ index.c
@@ -4,19 +4,19 @@
 int main(void)
 {
     setlocale(LC_ALL, "ru");
-    int n;
-    FILE *file = fopen("IN.txt", "w");
+    int n;//переменная для целых чисел
+    FILE *file = fopen("IN.txt", "w");//открываем файл
 
     puts("Привет, введи количество вещественных чисел, по-братски");
-    scanf("%d", &n);
+    scanf("%d", &n);//считываем кол-во вещественных чисел
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//заполняем вещественными числами
     {
         float temp;
         printf("Введите число:");
         scanf("%f", &temp);
 
-        fprintf(file, "%f ", temp);
+        fprintf(file, "%f ", temp);//печатаем число в файл
     }
     
     fclose(file);
